3b7a0b61711d81c184827fc888d1d51c15491acb
Carlos <carlos@Carloss-MBP.hsd1.ca.comcast.net>
2019-10-14 09:49:16 -0700

    nested functions Dominated

--- CS450_HMW3.py
+++ CS450_HMW3.py
@@ -1,3 +1,4 @@
+#Ex1___________________________________________________
 def incr(x):
   return (x + 1)
 def triple(x):
@@ -15,10 +16,38 @@ def intscts(f, x):
       return False
   return g
 
-at_three = intscts(square, 3)
-at_three(triple)
-at_three(incr)
-at_one = intscts(identity, 1)
-at_one(square)
-at_one(triple)
-  
+#at_three = intscts(square, 3)
+#at_three(triple)
+#at_three(incr)
+#at_one = intscts(identity, 1)
+#at_one(square)
+#at_one(triple)
+
+
+#Ex2___________________________________________________
+def add(g1, g2):
+  def g(y):
+    return g1(y) + g2(y)
+  return g
+
+identity = lambda x:x
+square = lambda x:x**2
+
+#a1 = add(identity, square)
+#a1(4)
+#a2 = add(a1, identity)
+#a2(4)
+#a2(5)
+#a3 = add(a1, a2)
+#a3(4)
+
+
+#Ex3___________________________________________________
+def f():
+  def g():
+    def h():
+      return 3
+    return h
+  return g
+
+f()()(3)
